31968d7f88cc20a53d47e3e4df5c6a12b3d88e1c
davMac606 <cl201238@g.unicamp.br>
2023-04-05 11:39:47 -0300

    motivo do suicidio: no body returned for response

--- src/index.ts
+++ src/index.ts
@@ -15,23 +15,39 @@ app.use(
 );
 
 app.get('/', (_, res) => {
-  res.send('Hello world!');
+  res.send('Have you ever heard the tragedy of <b>Count Matioli, the Italian</b>?');
 });
 
 app.get('/api/professores', (_, res) => {
   res.send(professores.map((professor) => ({ nome: professor })));
 });
 
-app.get('api/echo/:text', (req, res) => {
+app.get('/api/echo/:text', (req, res) => {
   res.send(req.params.text);
 });
 
-app.get('/api/frutas', (_, res) => {
-  connection.query('SELECT * FROM frutas').then(([rows]) => {
-    res.send(rows);
-  });
+app.get('/api/frutas', async (_, res) => {
+  const [rows] = await connection.query('SELECT * FROM testeImport');
+  res.send(rows);
+});
+
+app.post('/api/trust', (req, res) => {
+  
+  res.json(req.body);
+});
+
+const objetoPog = {
+  nome: "Matioli",
+  sobrenome: "The Italian",
+  idade: 616,
+  profissao: "Great-Duke Great-Lord Major Admiral of the Second Legion's forward artillery—cavalry multiplication of Devils."
+}
+
+app.get('/api/Matioli', (_, res) => {
+  res.send(objetoPog);
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
